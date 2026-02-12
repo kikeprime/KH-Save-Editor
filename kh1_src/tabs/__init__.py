431bb4933b0f13ae103bde845b0567f5cf66b791
Kiké fővezér <hermandavidtamas@gmail.com>
2026-02-12 21:57:22 +0100

    Add files via upload

---
+++ kh1_src/tabs/__init__.py
@@ -0,0 +1,7 @@
+from .general import *
+from .characters import *
+from .inventory import *
+from .journal import *
+from .config import *
+from .misc import *
+from .gummi import *
---
+++ kh1_src/tabs/misc.py
@@ -0,0 +1,4 @@
+from dash import Dash, html, dcc, callback, Input, Output, State, ALL
+import kh1_src.kh1_utils as utils
+
+
