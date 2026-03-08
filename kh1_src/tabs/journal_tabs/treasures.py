e4e402be78d1e86ffe8d8d697f8f9a778569f4a2
Kiké fővezér <hermandavidtamas@gmail.com>
2026-03-08 01:40:35 +0100

    Update kh1 journal_tabs/treasures.py

--- kh1_src/tabs/journal_tabs/treasures.py
+++ kh1_src/tabs/journal_tabs/treasures.py
@@ -22,6 +22,14 @@ def __create_treasures(tab):
             for k, v in kh1.treasure_dicts[tab].items()
         ])
     ])
+    unique = __create_treasures_unique(tab)
+    return html.Div([
+        treasures,
+        unique,
+    ])
+
+def __create_treasures_unique(tab):
+    kh1 = utils.kh1
     unique = None
     if tab == "Atlantica":
         clams = html.Div([
@@ -39,10 +47,29 @@ def __create_treasures(tab):
             html.H3("Clams:"),
             clams,
         ])
-    return html.Div([
-        treasures,
-        unique,
-    ])
+    if tab == "Neverland":
+        doors = html.Div([
+            html.Div([
+                dcc.Checklist(
+                    options=[{"label": k, "value": (1 << v % 16)}],
+                    value=[kh1.bigben[v//16] & (1 << v % 16)],
+                    id={"type": "Big Ben Door", "index": v},
+                    style={"margin-top": 10},
+                )
+            ])\
+            for k, v in kh1.bigben_dict.items()
+        ])
+        unique = html.Div([
+            dcc.Checklist(
+                options=[{"label": "Ship: Hold Aero Chest", "value": (1 << 1)}],
+                value=[kh1.bigben[1] & (1 << 1)],
+                id={"type": "Big Ben Door", "index": 0x11},
+                style={"margin-top": 10},
+            ),
+            html.H3("Big Ben Doors:"),
+            doors,
+        ])
+    return unique
 
 def create_treasures():
     kh1 = utils.kh1
@@ -86,3 +113,16 @@ def journal_treasures_clams_callback(values, ids):
             kh1.clams[v // 16] |= (1 << v % 16)
         else:
             kh1.clams[v // 16] &= ~(1 << v % 16)
+
+@callback(
+    Input({"type": "Big Ben Door", "index": ALL}, "value"),
+    State({"type": "Big Ben Door", "index": ALL}, "id"),
+)
+def journal_treasures_bigben_callback(values, ids):
+    kh1 = utils.kh1
+    for i in range(len(values)):
+        v = ids[i]["index"]
+        if (1 << v % 16) in values[i]:
+            kh1.bigben[v // 16] |= (1 << v % 16)
+        else:
+            kh1.bigben[v // 16] &= ~(1 << v % 16)
