c39dc24f2a7f857110ffaacf8ea0bf548bca975f
Kiké fővezér <hermandavidtamas@gmail.com>
2026-03-14 22:22:58 +0100

    Support Gummi Block OBJ model files
    I exported the models of the blocks used in the default model using Hypercrown v0.2.0 (the latest free version) and the function added converts them to be used with the viewer. Cure-G lacks the dome and Thunder-G is barely recognizable.
    Rotation isn't implemented since I need to research it in the game 1st.
    I'll only add the models when I learned the rotations. However, it works as before since I put fallbacks.

--- kh1_src/kh1_gummi_viewer.py
+++ kh1_src/kh1_gummi_viewer.py
@@ -1,17 +1,39 @@
 from dash import Dash, html, dcc, callback, Input, Output, State, ALL
 import kh1_src.kh1_utils as utils
 import numpy as np
+import pandas as pd
 import plotly.graph_objects as go
 
 
+def obj_to_mesh3d(d):
+    n = {0: 1, 58: np.array([1, 1.5, 0.5])}
+    for i in range(96):
+        try:
+            df = pd.read_csv(f"assets/gummi/gumi-s0-{i}.obj", header=None, delimiter=" ")
+            v = df[df[0] == "v"][[1, 2, 3]].astype(float).to_numpy() / 200 + (n[i] if i in n else 0.5)
+            f = df[df[0] == "f"][[1, 2, 3]].map(lambda x: int(x.split("/")[0])).astype(int).to_numpy() - 1
+            d[i+1] = {"v": v, "f": f}
+        except:
+            continue
+
 def viewer_callback(idx):
     kh1 = utils.kh1
-    #print(kh1.gummiships[idx].name.decode("kh1us"))
+    d = {}
+    obj_to_mesh3d(d)
     fig = go.Figure()
     for i in range(kh1.gummiships[idx].blockcount.value):
         block = kh1.gummiships[idx].blocks[i]
         color = ["lightgray", "yellow", "orange", "red"][block.color]
         fig.add_trace(
+            go.Mesh3d(
+                x=d[block.id]["v"][:,0]+block.x,
+                y=d[block.id]["v"][:,1]+block.y,
+                z=d[block.id]["v"][:,2]+block.z,
+                i=d[block.id]["f"][:,0],
+                j=d[block.id]["f"][:,1],
+                k=d[block.id]["f"][:,2],
+                color=color,
+            ) if block.id in d else\
             go.Mesh3d(
                 x=np.array([0,1,0,0,1,1,0,1])+block.x,
                 y=np.array([0,0,1,0,1,0,1,1])+block.y,
