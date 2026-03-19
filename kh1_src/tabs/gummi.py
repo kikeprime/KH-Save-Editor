03fbd1a4f75a081f74587e6d78d27bf7278012e8
Kiké fővezér <hermandavidtamas@gmail.com>
2026-03-19 01:23:26 +0100

    Gummi Ship editing
    The known properties of the Gummi Ships can now be edited except for Gummi Blocks which will come in a later update.

--- kh1_src/tabs/gummi.py
+++ kh1_src/tabs/gummi.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc, callback, Input, Output, State, ALL
+from dash import Dash, html, dcc, callback, Input, Output, State, ALL, MATCH
 import kh1_src.kh1_utils as utils
 from kh1_src.kh1_gummi_viewer import *
 
@@ -36,6 +36,101 @@ def __create_gummi_ships(encoding):
         html.Div(id="ShipDiv", style={"margin-bottom": 20}),
         viewerb,
         html.Div(id="ViewerDiv", style={"margin-bottom": 20}),
+    ])
+
+@callback(
+    Output("ShipDiv", "children"),
+    Input("ShipTabs", "value"),
+    Input("Encoding", "value"),
+)
+def __create_ships(idx, encoding):
+    kh1 = utils.kh1
+    ship = kh1.gummiships[idx]
+    blockcount = dcc.Input(
+        id={"type": "Block Count", "index": idx},
+        type="number",
+        value=ship.blockcount.value,
+        min=0,
+        max=200,
+        step=1,
+        style={"width": 50},
+    )
+    area = html.Div([
+        html.Label("X: "),
+        dcc.Input(
+            id={"type": "Assembly Area X", "index": idx},
+            type="number",
+            value=ship.x.value,
+            min=0,
+            max=10,
+            step=1,
+            style={"width": 50},
+        ),
+        html.Label(" Y: "),
+        dcc.Input(
+            id={"type": "Assembly Area Y", "index": idx},
+            type="number",
+            value=ship.y.value,
+            min=0,
+            max=10,
+            step=1,
+            style={"width": 50},
+        ),
+        html.Label(" Z: "),
+        dcc.Input(
+            id={"type": "Assembly Area Z", "index": idx},
+            type="number",
+            value=ship.z.value,
+            min=0,
+            max=10,
+            step=1,
+            style={"width": 50},
+        ),
+    ])
+    codec = "kh1us" if encoding == "International" else "kh1jp"
+    transformpair = dcc.Dropdown(
+        options=[
+            {"label": "No Pair", "value": 0}
+        ] + [
+            {"label": kh1.gummiships[i].name.decode(codec), "value": i+1} for i in range(len(kh1.gummiships))
+        ],
+        value=ship.transformpair.value,
+        id={"type": "Transform Pair", "index": idx},
+        style={"margin-bottom": 10, "width": 200},
+        searchable=False,
+        clearable=False,
+    )
+    name = html.Div([
+        dcc.Input(
+            id={"type": "Ship Name", "index": idx},
+            type="text",
+            value=ship.name.decode(codec),
+            style={"width": 120},
+        ),
+        html.Button(
+            "Validate", id={"type": "Ship Name Validate", "index": idx}, n_clicks=0, style={"width": 80, "margin-left": 10},
+        ),
+    ])
+    blocktabs = dcc.Dropdown(
+        options=[{"label": f"Block {i+1}", "value": i} for i in range(len(ship.blocks))],
+        value=0,
+        id="BlockTabs",
+        style={"margin-bottom": 10, "width": 200},
+        searchable=False,
+        clearable=False,
+    )
+    return html.Div([
+        dcc.Markdown("Block Count:"),
+        blockcount,
+        dcc.Markdown("Assembly Area:"),
+        area,
+        dcc.Markdown("Transform Pair:"),
+        transformpair,
+        dcc.Markdown("Name:"),
+        name,
+        dcc.Markdown("Blocks:"),
+        blocktabs,
+        html.Div(id="BlockDiv"),
     ])
 
 @callback(
@@ -258,6 +353,45 @@ def create_gummi():
         gtabs,
         html.Div(id="GummiDiv"),
     ])
+
+@callback(
+    Input({"type": "Block Count", "index": ALL}, "value"),
+    Input({"type": "Assembly Area X", "index": ALL}, "value"),
+    Input({"type": "Assembly Area Y", "index": ALL}, "value"),
+    Input({"type": "Assembly Area Z", "index": ALL}, "value"),
+    Input({"type": "Transform Pair", "index": ALL}, "value"),
+    State({"type": "Block Count", "index": ALL}, "id"),
+)
+def ship_callback(blockcount, x, y, z, transformpair, id):
+    kh1 = utils.kh1
+    idx = id[0]["index"]
+    ship = kh1.gummiships[idx]
+    try:
+        ship.blockcount.value = blockcount[0]
+        ship.x.value = x[0]
+        ship.y.value = y[0]
+        ship.z.value = z[0]
+        ship.transformpair.value = transformpair[0]
+    except:
+        pass
+
+@callback(
+    Output({"type": "Ship Name", "index": MATCH}, "value"),
+    Input({"type": "Ship Name Validate", "index": MATCH}, "n_clicks"),
+    State({"type": "Ship Name", "index": MATCH}, "value"),
+    State({"type": "Ship Name", "index": MATCH}, "id"),
+    State("Encoding", "value"),
+)
+def ship_name_callback(n_clicks, name, id, encoding):
+    kh1 = utils.kh1
+    codec = "kh1us" if encoding == "International" else "kh1jp"
+    idx = id["index"]
+    if n_clicks > 0:
+        new_name = bytearray(name, codec)
+        l = len(new_name)
+        kh1.gummiships[idx].name[:min(l, 10)] = new_name[:min(l, 10)]
+        return kh1.gummiships[idx].name.decode(codec)
+    return name
 
 @callback(
     Input({"type": "Gummi Block", "index": ALL}, "value"),
