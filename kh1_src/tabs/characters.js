116e04d1a0573c3a32f8a3d6ddf3e3d52139ab07
kikeprime <hermandavidtamas@gmail.com>
2026-06-21 22:38:13 +0000

    Add missing files

---
+++ kh1_src/tabs/characters.js
@@ -0,0 +1,250 @@
+export function create_characters() {
+    const kh1div = document.getElementById("kh1div");
+    const ignored = new Set(["Winnie the Pooh", "None"]);
+    const char_options = Object.entries(window.kh1.character_dict)
+        .filter(([label]) => !ignored.has(label))
+        .map(([label, value]) => `\n\t<option value=${value}>${label}</option>`)
+        .join("");
+    kh1div.innerHTML = `
+    <div>
+        <div style="display: flex; gap: 20px">
+            <select id="kh1chartabs">
+                ${char_options}
+            </select>
+            <select id="kh1charsubtabs">
+                <option value="Stats">Stats</option>
+                <option value="Equipment">Equipment</option>
+                <option value="Abilities">Abilities</option>
+                <option value="Customize">Customize</option>
+                <option value="Shared Abilities">Shared Abilities</option>
+            </select>
+        </div>
+        <div id="kh1chardiv"></div>
+    </div>`;
+    const kh1chartabs = document.getElementById("kh1chartabs");
+    const kh1charsubtabs = document.getElementById("kh1charsubtabs");
+    function tab_sel() {
+        create_character(kh1chartabs.value, kh1charsubtabs.value);
+    }
+    kh1chartabs.addEventListener("change", tab_sel);
+    kh1charsubtabs.addEventListener("change", tab_sel);
+    create_character(kh1chartabs.value, kh1charsubtabs.value);
+}
+
+function create_character(id, tab) {
+    switch (tab) {
+        case "Stats": {
+            create_stats(id);
+            break;
+        }
+        default: {
+            const kh1chardiv = document.getElementById("kh1chardiv");
+            kh1chardiv.innerHTML = "";
+            break;
+        }
+    }
+}
+
+function create_stats(id) {
+    const kh1chardiv = document.getElementById("kh1chardiv");
+    const c = window.kh1.characters[id];
+    const exp = `
+    <div>
+        <h3>EXP:</h3>
+        <input
+            type="number"
+            id="exp"
+            min=0
+            max=999999
+            step=1
+            value=${c.exp.value}
+        >
+    </div>`;
+    const level = `
+    <div>
+        <h3>Level:</h3>
+        <input
+            type="number"
+            id="level"
+            min=1
+            max=100
+            step=1
+            value=${c.level.value}
+        >
+    </div>`;
+    const hp = `
+    <div>
+        <h3>HP:</h3>
+        <input
+            type="number"
+            id="hp"
+            min=0
+            max=255
+            step=1
+            value=${c.hp.value}
+        >
+    </div>`;
+    const maxhp = `
+    <div>
+        <h3>Max HP:</h3>
+        <input
+            type="number"
+            id="maxhp"
+            min=0
+            max=255
+            step=1
+            value=${c.maxhp.value}
+        >
+    </div>`;
+    const mp = `
+    <div>
+        <h3>MP:</h3>
+        <input
+            type="number"
+            id="mp"
+            min=0
+            max=255
+            step=1
+            value=${c.mp.value}
+        >
+    </div>`;
+    const maxmp = `
+    <div>
+        <h3>Max MP:</h3>
+        <input
+            type="number"
+            id="maxmp"
+            min=0
+            max=255
+            step=1
+            value=${c.maxmp.value}
+        >
+    </div>`;
+    const maxap = `
+    <div>
+        <h3>Max AP:</h3>
+        <input
+            type="number"
+            id="maxap"
+            min=0
+            max=255
+            step=1
+            value=${c.maxap.value}
+        >
+    </div>`;
+    const strength = `
+    <div>
+        <h3>Strength:</h3>
+        <input
+            type="number"
+            id="strength"
+            min=0
+            max=255
+            step=1
+            value=${c.strength.value}
+        >
+    </div>`;
+    const defense = `
+    <div>
+        <h3>Defense:</h3>
+        <input
+            type="number"
+            id="defense"
+            min=0
+            max=255
+            step=1
+            value=${c.defense.value}
+        >
+    </div>`;
+    function magcheck(b) {
+        return c.magic.value & (1 << b) ? "checked" : "";
+    }
+    let magbox = "";
+    for (let i = 0; i < 7; i++) {
+        magbox += `
+        <label style="display: flex; align-items: center">
+            <input type="checkbox" value=${1 << i} ${magcheck(i)}>
+            ${window.kh1.magicnames[i]}
+        </label>`
+    }
+    const magic = `
+    <div id="magic">
+        <h3>Spells:</h3>
+        ${magbox}
+    </div>`;
+    const res = Object.entries(window.kh1.resistance_dict)
+        .map(([label, value]) => `
+        <label style="display: flex; gap: 10px; align-items: center">
+            ${label}: 
+            <input
+                type="number"
+                min=-100
+                max=100
+                step=1
+                value=${100 - c.resistances[value]}
+                name=${value}
+            > %
+        </label>`)
+        .join("");
+    const resistances = `
+    <div id="resistances">
+        <h3>Resistances:</h3>
+        ${res}
+    </div>`;
+    kh1chardiv.innerHTML = `
+    <div>
+        <div style="display: flex; gap: 20px">
+            ${exp}
+            ${level}
+        </div>
+        <div style="display: flex; gap: 20px">
+            ${hp}
+            ${maxhp}
+        </div>
+        <div style="display: flex; gap: 20px">
+            ${mp}
+            ${maxmp}
+        </div>
+        ${maxap}
+        <div style="display: flex; gap: 20px">
+            ${strength}
+            ${defense}
+        </div>
+        ${magic}
+        ${resistances}
+    </div>`;
+    stats_callbacks(id);
+}
+
+function stats_callbacks(id) {
+    const c = window.kh1.characters[id];
+    const exp = document.getElementById("exp");
+    exp.addEventListener("change", () => {
+        if (exp.validity.valid)
+            c.exp.value = exp.value;
+        exp.value = c.exp.value;
+    });
+    const level = document.getElementById("level");
+    level.addEventListener("change", () => {
+        if (level.validity.valid)
+            c.level.value = level.value;
+        level.value = c.level.value;
+    });
+    const magic = document.getElementById("magic");
+    magic.addEventListener("change", (e) => {
+        if (e.target.type === "checkbox") {
+            if (e.target.checked)
+                c.magic.value |= e.target.value
+            else
+                c.magic.value &= ~e.target.value
+        }
+    });
+    const resistances = document.getElementById("resistances");
+    resistances.addEventListener("change", (e) => {
+        if (e.target.type === "number") {
+            if (e.target.validity.valid)
+                c.resistances[e.target.name] = 100 - e.target.value
+            e.target.value = 100 - c.resistances[e.target.name]
+        }
+    });
+}
